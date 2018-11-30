84bdc023acb9c80967b27eeab24abe56448099cd
ChatRobot <1010472380@qq.com>
2018-11-30 10:50:22 +0800

    Add files via upload

--- tuling.py
+++ tuling.py
@@ -4,43 +4,68 @@
 
 import requests
 import json
-import logging
 
 from uuid import getnode as get_mac
 
+from location import Location
 
-class TulingRobot():
+"""
+图灵机器人
+"""
+class TulingRobot(object):
     def __init__(self):
         self.__apiKey = self.__get_apikey()
         self.__userId = self.__get_userId()
+        self.__province, self.__city= self.__get_Location()
 
     def __get_apikey(self):# apiKey,机器人标识
         return 'a323758630ff4d6dacf72d90b316fc3c'
     
     def __get_userId(self):# userId,用户唯一标识
         return str(get_mac())[:32]
+    
+    def __get_Location(self):# 获取地理位置
+        try:
+            location = Location()
+            country, province, city = location.get_Location()
+            return province, city
+        except:
+            return '',''
 
     def __format_msg(self, msg):
         msg = ''.join(msg)
-        body = {
-            "reqType": 0,
-            "perception": {
-                "inputText": {
-                    "text": msg
-                },
-                "selfInfo": {
-                    "location": {
-                        "city": "上海",
-                        "province": "上海",
-                        "street": "上海"
+        if len(self.__city) > 0:
+            body = {
+                "reqType": 0,
+                "perception": {
+                    "inputText": {
+                        "text": msg
+                    },
+                    "selfInfo": {
+                        "location": {
+                            "city": self.__city,
+                            "province": self.__province
+                        }
                     }
+                },
+                "userInfo": {
+                    "apiKey": self.__apiKey,
+                    "userId": self.__userId
                 }
-            },
-            "userInfo": {
-                "apiKey": self.__apiKey,
-                "userId": self.__userId
             }
-        }
+        else:
+            body = {
+                "reqType": 0,
+                "perception": {
+                    "inputText": {
+                        "text": msg
+                    }
+                },
+                "userInfo": {
+                    "apiKey": self.__apiKey,
+                    "userId": self.__userId
+                }
+            }
         return json.dumps(body)
 
     def chat(self, msg):
@@ -57,16 +82,3 @@ class TulingRobot():
             return mesage
         except Exception:
             return u'抱歉, 我的大脑短路了,请稍后再试试...'
-
-
-if __name__ == '__main__':
-    Robot = TulingRobot()
-
-    print("Now u can type in something & input q to quit")
-    while True:
-        msg = raw_input("\nMaster:")
-        if msg == 'q':
-            exit("u r quit the chat !")         # 设定输入q，退出聊天。
-        else:
-            turing_data = Robot.chat(msg)
-            print "Robot:%s" % (turing_data)
