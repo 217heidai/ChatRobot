60a958a3b39ac340cf45d52aecb4c968630e7b94
ChatRobot <1010472380@qq.com>
2018-12-03 19:12:53 +0800

    Add files via upload

--- chatrobot.py
+++ chatrobot.py
@@ -2,22 +2,51 @@
 # -*- encoding=utf-8 -*- s
 # 文件名：chatrobot.py
 
+import os
+
+from baidustt import BaiduSTT
+from baidutts import BaiduTTS
+from tools import Tools
 from tuling import TulingRobot
+from voice import Voice
+
 
 class ChatRobot(object):
     def __init__(self):
-        self.__tuling = TulingRobot()
-
-    def chat(self):
-        print("Now u can type in something & input q to quit")
+        self.__tools = Tools()
+        self.__baiduSTT = BaiduSTT() # 语音转文字模块
+        self.__baiduTTS = BaiduTTS() # 文字转语音模块
+        self.__voice = Voice() # 音频抓取模块
+        self.__tuling = TulingRobot() # 图灵机器人
+        
+    def textchat(self):
+        print("Now u can type in something & input q to quit\n")
         while True:
-            msg = raw_input("\nMaster:")
-            if msg == 'q':
+            """"文字聊天"""
+            request = raw_input("Master:")
+            if request == 'q':
                 exit("u r quit the chat !")         # 设定输入q，退出聊天。
             else:
-                request = self.__tuling.chat(msg)
-                print "Robot:%s" % (request)
+                response = self.__tuling.chat(request)
+                print "Robot:%s" % (response)
+
+    def voicechat(self): 
+        print("Now u can talk to me\n")
+        while True:
+            """"语音聊天"""
+            speechFile = self.__voice.get_voice()
+            if os.path.exists(speechFile):
+                request = self.__baiduSTT.STT(speechFile)
+                print request
+                response = self.__tuling.chat(request)
+                print response
+                speechFile = self.__baiduTTS.TTS(response)
+                if os.path.exists(speechFile):
+                    self.__tools.play_mp3(speechFile)
+                    os.remove(speechFile)
+
 
 if __name__ == '__main__':
     chatRobot = ChatRobot()
-    chatRobot.chat()
+    chatRobot.textchat()
+    #chatRobot.voicechat()
